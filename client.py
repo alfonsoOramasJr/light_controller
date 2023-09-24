ee03d774f9de45b95960fc5d395573e4ebdae5ba
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-09-24 12:05:25 -0400

    established some debugging messages.

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import socket
 
 def createClient():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("CLIENT OK")
     return client
 
 def getData(client):
@@ -10,6 +11,7 @@ def getData(client):
 def main():
     client = createClient()
     client.connect(('10.0.0.138', 3000))
+    print("CONNECTION OK")
 
     while True:
         data = getData(client)
